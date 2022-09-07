d91c41387440268eaeab27f2c04e39842719e384
kirangautam45 <gautamkiran38@gmail.com>
2022-09-07 11:27:06 +0545

    jsonData Load

---
+++ src/Stocks.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./App.css";
+import { stockData } from "./data";
+
+export const Stocks = () => {
+  return (
+    <>
+      <div className="header">
+        <h2>Your Stock Tracker</h2>
+      </div>
+      <div className="stock-container">
+        {stockData.map((data, key) => {
+          return (
+            <div key={key}>
+              <Stock
+                key={key}
+                company={data.company}
+                ticker={data.ticker}
+                stockPrice={data.stockPrice}
+                timeElapsed={data.timeElapsed}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const Stock = (props) => {
+  if (!props.company) return <div />;
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            <h5>{props.company}</h5>
+          </td>
+          <td>
+            <h5>{props.ticker}</h5>
+          </td>
+          <td>
+            <h4>{props.stockPrice}</h4>
+          </td>
+          <td>
+            <p>{props.timeElapsed}</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
---
+++ src/serviceWorker.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const serviceWorker = () => {
+  return (
+    <div>serviceWorker</div>
+  )
+}
